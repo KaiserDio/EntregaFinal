2b1b5fa8fd883a235ffe1367308775b8e7041a90
BejaranoSantiago <37586477+BejaranoSantiago@users.noreply.github.com>
2022-04-29 14:39:52 -0300

    Se suman Modelos y se crea el administrador

--- EntregaFinal/ECommerce/admin.py
+++ EntregaFinal/ECommerce/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cliente, Vendedor, Producto, Compra, CompraXProducto, Mensaje, Novedad
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Vendedor)
+admin.site.register(Producto)
+admin.site.register(Compra)
+admin.site.register(CompraXProducto)
+admin.site.register(Mensaje)
+admin.site.register(Novedad)
--- EntregaFinal/ECommerce/models.py
+++ EntregaFinal/ECommerce/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    telefono = models.IntegerField()
+    dni = models.IntegerField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - E-Mail {self.email}"
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    dni = models.IntegerField()
+    cuil = models.IntegerField()
+    salario = models.IntegerField()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - E-Mail {self.email}"
+
+class Mensaje(models.Model):
+    idMensajeAnterior = models.IntegerField()
+    idCliente = models.ForeignKey(Cliente, on_delete = models.PROTECT)
+    idVendedor = models.ForeignKey(Vendedor, on_delete = models.PROTECT)
+    asunto = models.CharField(max_length=150)
+    mensaje = models.CharField(max_length=5000)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"idCliente: {self.idCliente} - idVendedor: {self.idVendedor} - Asunto {self.asunto}"
+
+class Compra(models.Model):
+    idCliente = models.ForeignKey(Cliente, on_delete = models.PROTECT)
+    idVendedor = models.ForeignKey(Vendedor, on_delete = models.PROTECT)
+    direccion = models.CharField(max_length=200)
+    localidad = models.CharField(max_length=150)
+    codigoPostal = models.IntegerField()
+    estado = models.CharField(max_length=150)
+    medioPago = models.CharField(max_length=200)
+    fechaCompra = models.DateField()
+    fechaEnvio = models.DateField()
+    fecha = models.DateField()
+    comprobantePago = models.IntegerField()
+
+    def __str__(self):
+        return f"Compra de: {self.idCliente} - Fecha de Compra: {self.fechaCompra} - E-Mail {self.email}"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=40)
+    categoria = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=5000)
+    imagen = models.ImageField()
+    stock = models.IntegerField()
+    precio = models.IntegerField()
+
+class CompraXProducto(models.Model):
+    idCompra = models.ForeignKey(Compra, on_delete= models.CASCADE)
+    idProducto = models.ForeignKey(Producto, on_delete= models.CASCADE)
+    cantidad = models.IntegerField()
+    precioTotal = models.IntegerField()
+
+class Novedad(models.Model):
+    titulo = models.CharField(max_length=150)
+    subtitulo = models.CharField(max_length=150)
+    contenido = models.CharField(max_length=5000)
+    imagen = models.ImageField()
+    fecha = models.DateField()
